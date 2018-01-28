549307c590d0824073476d4ffecbd68ba7cfbaf2
enzozafra <zafra@ualberta.ca>
2018-01-28 01:35:12 -0700

    REST functions almost done I need REST :'(

--- app/app.js
+++ app/app.js
@@ -1,6 +1,10 @@
 var testdata = require('../data.json');
 var coursedata = require('../data2.json');
 
+var DEPARTMENT_COLLECTION = 'department';
+var SPECIALIZATION_COLLECTION = 'specialization';
+var COURSE_COLLECTION = 'course';
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -64,55 +68,133 @@ function getPreReqs(request, response) {
 
 app.get('/search/departments/', getAllDepartments);
 
-function getAllDepartments(request, response) {
-  var reply;
-  var data = testdata.department.map(value => value.name);
+function getAllDepartments(req, res) {
+  db.collection(DEPARTMENT_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get departments.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
+}
 
-  reply = {
-    status: "found",
-    data: { "departments": data }
-  }
-  response.send(reply);
+app.get('/search/specializations/', getAllSpecializations);
+
+function getAllSpecializations(req, res) {
+  db.collection(SPECIALIZATION_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get specializations.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 }
 
 app.get('/search/courses/', getAllCourses);
 
 function getAllCourses(request, response) {
-  //TODO return all courses instaed of just data;
-  // need temp json file that has courses
-  var reply;
-  var data = coursedata;
-
-  reply = {
-    status: "found",
-    data: data
-  }
-  response.send(reply);
+  db.collection(COURSE_COLLECTION).find({}).toArray(function(err, docs) {
+    if (err) {
+      handleError(res, err.message, "Failed to get courses.");
+    } else {
+      res.status(200).json(docs);
+    }
+  });
 }
 
-app.get('/search/course/:coursename', getCourse);
+app.get('/search/course/', getCourse);
 
-function getCourse(request, response) {
-  var reply;
-  var course = request.params.coursename;
+function getCourse(req, res) {
+    db.collection(COURSE_COLLECTION).findOne({name: req.query.name}, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get course.");
+        return;
+      } else {
+        res.status(200).json(doc);
+      }
+    });
+}
+
+app.get('/search/department/', getDepartment);
+
+function getDepartment(req, res) {
+    db.collection(DEPARTMENT_COLLECTION).findOne({name: req.query.name}, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get department.");
+        return;
+      } else {
+        res.status(200).json(doc);
+      }
+    });
+}
+
+app.get('/search/specialization/', getSpecialization);
+
+function getSpecialization(req, res) {
+    db.collection(SPECIALIZATION_COLLECTION).findOne({name: req.query.name}, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get specialization.");
+        return;
+      } else {
+        res.status(200).json(doc);
+      }
+    });
 }
 
 app.post('/insert/department/', insertDepartment);
 
 function insertDepartment(req, res) {
-  console.log(req.body);
   if (!req.body.name) {
     handleError(res, "Invalid user input", "Must provide a name.", 400);
   } else {
     var department = {
-      "name" : req.name,
+      "name" : req.body.name,
       "specializations": []
     };
 
-    db.collection("department").insertOne(department, function(err, doc) {
+    db.collection(DEPARTMENT_COLLECTION).insertOne(department, function(err, doc) {
       if (err) {
         handleError(res, err.message, "Failed to create new department.");
       } else {
+        res.status(201).json(doc.ops[0]);
+      }
+    });
+  }
+}
+
+app.post('/insert/specialization/', insertSpecialization);
+
+function insertSpecialization(req, res) {
+  var department;
+  if (!req.body.name || !req.body.department ) {
+    handleError(res, "Invalid user input", "Must provide a specialization name and department.", 400);
+  } else {
+
+    // Get department collection
+    db.collection(DEPARTMENT_COLLECTION).findOne({name: req.body.department}, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to get department.");
+        return;
+      } else {
+        department = doc;
+      }
+    });
+
+    var specialization = {
+      "name" : req.body.name,
+      "courses": []
+    };
+
+    db.collection(SPECIALIZATION_COLLECTION).insertOne(specialization, function(err, doc) {
+      if (err) {
+        handleError(res, err.message, "Failed to create new specialization.");
+      } else {
+        department.specializations.push(doc.ops[0]._id);
+        db.collection(DEPARTMENT_COLLECTION).updateOne(
+          { '_id' : department._id },
+          { $set: { "specializations" : department.specializations } }
+        );
+        console.log(department)
         res.status(201).json(doc.ops[0]);
       }
     });
