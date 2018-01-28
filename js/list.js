2013b84952d138dc7325f41bac182809fb3323e5
Abid Rahman <arahman1@ualberta.ca>
2018-01-28 02:44:32 -0700

    Add prereq highlighting to sidebar list

--- js/list.js
+++ js/list.js
@@ -30,7 +30,7 @@ $( document ).ready(function () {
 
     function renderList(num) {
         d3.json("../data.json", function(error, data) {
-            var courses = data.department[num].specialization[0].children;
+            courses = data.department[num].specialization[0].children;
             $('ul').remove();
             $('.course-list').append('<ul/>');
             $.each(courses, function() {
@@ -38,10 +38,23 @@ $( document ).ready(function () {
                     listItem = $('<li class="course-list-item"/>'),
                     html = listItem.append($('<p/>').text(this.name));
                 list.append(html);
-                listItem.mouseover(function() {
-                    console.log(this);
-                });
+                listItem.mouseover(function() { showInfo($(this)); });
+                listItem.mouseout(function() { $('li').css('background-color', '#eee'); });
             });
         });
     }
+
+    function showInfo(course) {
+        course.css('background-color', '#ddd');
+        courses.forEach(function(d) {
+            if (d.name == course[0].innerText) {
+                d.children.forEach(function(d) {
+                    console.log(d.name);
+                    $('li:contains('+ d.name + ')').css('background-color', 'rgba(255,0,0,0.6)');
+                });
+            }
+        });
+    }
+
+
 });
